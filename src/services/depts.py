adf845e66dd49f61b9f24accdef9e7aceccfa47b
Maltsev.DM <dimamalcev777@gmail.com>
2024-02-27 15:15:52 +0500

    fix: Поменять имя репозитория счетов.

--- src/services/depts.py
+++ src/services/depts.py
@@ -1,5 +1,4 @@
 from src.schemas.debts import DebtAdd, DebtRead, DebtUpdate
-from src.utils.enum_classes import CategoryGroup
 from src.utils.unit_of_work import IUnitOfWork
 
 
@@ -12,7 +11,7 @@ class DebtsService:
             debt_dict['user_id'] = user_id
             db_debt = await uow.debts.add_one(debt_dict)
 
-            await self._update_categories_card_balance(
+            await self._update_banks_card_balance(
                 uow, user_id,
                 id_bank_new=debt.bank_id, amount_new=debt.amount
             )
@@ -26,7 +25,7 @@ class DebtsService:
         async with uow:
             db_debt = await uow.debts.drop_one(id=id, user_id=user_id)
 
-            await self._update_categories_card_balance(
+            await self._update_banks_card_balance(
                 uow, user_id,
                 id_bank_old=db_debt.bank_id, amount_old=db_debt.amount
             )
@@ -40,7 +39,7 @@ class DebtsService:
             return [DebtRead.model_validate(x, from_attributes=True)
                     for x in debts]
 
-    async def _update_categories_card_balance(
+    async def _update_banks_card_balance(
             self, uow, user_id: int,
             id_bank_old: int | None = None,
             amount_old: float | None = None,
@@ -48,19 +47,18 @@ class DebtsService:
             amount_new: float | None = None
     ):
         if id_bank_old is not None and amount_old is not None:
-            db_bank_old = await uow.categories.find_one(
-                id=id_bank_old, user_id=user_id,
-                group=CategoryGroup.bank)
+            db_bank_old = await uow.banks.find_one(
+                id=id_bank_old, user_id=user_id)
             db_bank_old.credit_card_balance += amount_old
             if id_bank_new is None and amount_new is not None:
                 db_bank_old.credit_card_balance -= amount_new
 
         if id_bank_new is not None:
-            db_bank_new = await uow.categories.find_one(
-                id=id_bank_new, user_id=user_id,
-                group=CategoryGroup.bank)
+            db_bank_new = await uow.banks.find_one(
+                id=id_bank_new, user_id=user_id)
 
-            db_bank_new.credit_card_balance -= amount_old if amount_new is None else amount_new
+            db_bank_new.credit_card_balance -= (amount_old if amount_new is None
+                                                else amount_new)
 
     async def update_one(
             self, uow: IUnitOfWork, id: int, user_id: int, debt: DebtUpdate
@@ -71,20 +69,20 @@ class DebtsService:
 
             if debt.amount is not None and debt.bank_id is not None:
                 # Если обновились счёт и сумма
-                await self._update_categories_card_balance(
+                await self._update_banks_card_balance(
                     uow, id_bank_old=db_debt.bank_id,
                     id_bank_new=debt.bank_id, amount_old=db_debt.amount,
                     amount_new=debt.amount, user_id=user_id)
 
             elif debt.amount is not None:
                 # Если обновилась только сумма
-                await self._update_categories_card_balance(
+                await self._update_banks_card_balance(
                     uow, id_bank_old=db_debt.bank_id, amount_old=db_debt.amount,
                     amount_new=debt.amount, user_id=user_id)
 
             elif debt.bank_id is not None:
                 # Если обновился только счёт
-                await self._update_categories_card_balance(
+                await self._update_banks_card_balance(
                     uow, id_bank_old=db_debt.bank_id,
                     id_bank_new=debt.bank_id, amount_old=db_debt.amount,
                     user_id=user_id)
